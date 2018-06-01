a551a428f65cd005062757ed41a3d909aa65e9bb
vilas <jlvilas@cnb.csic.es>
2018-06-01 15:16:02 +0200

    monodir: fixing tangencial and azimuthal resolution representation

--- pyworkflow/em/packages/xmipp3/viewer_resolution_directional.py
+++ pyworkflow/em/packages/xmipp3/viewer_resolution_directional.py
@@ -202,7 +202,9 @@ class XmippMonoDirViewer(ProtocolViewer):
             fig, im = self._plotVolumeSlices(titleFigure, imgData2,
                                          0, 1, self.getColorMap(), dataAxis=self._getAxis())
         else:
-            max_Res = np.percentile(imgData2, 95)
+            maskNan = np.isnan(imgData2)
+            goodResolutions = np.ma.masked_array(imgData2, mask=maskNan)
+            max_Res = np.percentile(goodResolutions, 95)
             print max_Res
 #             max_Res = np.nanmax(imgData2)
             min_Res = np.nanmin(imgData2)
